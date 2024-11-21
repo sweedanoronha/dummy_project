27f4cff3a1df0da16bf61bbeed850513c0beab9f
sweeda@anoralabs.com <sweeda@anoralabs.com>
2024-11-21 14:15:35 +0530

    testing

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "query": q}
+
+@app.get("/item/{item_id}")
+def read_items(item_id: int, q: str = None):
+    return {"item_id": item_id, "query": q}
